7a6c59aeb06585d3e391cfa7c9dc03e393db0b17
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-05-25 11:30:47 +0700

    membuat descending

--- Doublelinklist.cpp
+++ Doublelinklist.cpp
@@ -89,6 +89,25 @@ bool DoubleLinkList::deleteNode(int rollNo) {
 bool DoubleLinkedList::listEmpty() {
     return (START == NULL);
 }
+void DoubleLinkedList::ascending() {
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else {
+        cout << "\Records in ascending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+void DoubleLinkedList::descending() {
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else {
+        cout<<"\nRecords in descending order of roll number are"
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
