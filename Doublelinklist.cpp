46c6453630605ac854555db3e209221f70d395cc
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-05-25 10:52:16 +0700

    jika new node next maka menjadi start

--- Doublelinklist.cpp
+++ Doublelinklist.cpp
@@ -41,7 +41,14 @@ void DoubleLinkedList::addNode(){
     if (START == NULL || nim <= START->noMhs) {
         if (START != NULL && nim == START->noMhs) {
             cout << "\nDuplicate number not allowed" << endl;
+            return;
         }
+        newNode->next = START;
+        if (START != NULL)
+            START->prev = newNode;
+        newNode->prev = NULL;
+        START = newNode;
+        return;
      }
 }
 int main()
